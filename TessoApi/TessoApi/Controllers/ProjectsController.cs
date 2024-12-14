a2f74c59da05a426b6870800ea1833410f352998
Sudhansu Kumar Maharana <sudhansumaharana812@gmail.com>
2024-12-14 11:36:30 +0530

    Revert "CRUD operations to Projects"
    This reverts commit 0c43f3078012fed33567a4f7d7d3ac4cdf3e63b8.

--- TessoApi/TessoApi/Controllers/ProjectsController.cs
+++ TessoApi/TessoApi/Controllers/ProjectsController.cs
@@ -3,8 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 using TessoApi.DTOs.Project;
 using TessoApi.Services.Interfaces;
 using TessoApi.Models.Http;
-using Azure;
-using Microsoft.AspNetCore.JsonPatch;
 
 namespace TessoApi.Controllers
 {
@@ -17,7 +15,7 @@ namespace TessoApi.Controllers
         public async Task<IActionResult> Create([FromBody] CreateProjectDto project)
         {
             CustomHttpResponse<ReadProjectDto> response = await _projectService.Create(project);
-            return StatusCode((int)response.StatusCode, response.Data);
+            return StatusCode((int) response.StatusCode, response.Data);
         }
 
         [HttpGet]
@@ -31,27 +29,6 @@ namespace TessoApi.Controllers
         public async Task<IActionResult> GetUserProjects([FromQuery] Guid userId)
         {
             CustomHttpResponse<List<ReadProjectDto>> response = await _projectService.GetUserProjects(userId);
-            return StatusCode((int)response.StatusCode, response.Data);
-        }
-
-        [HttpPut("{projectId}")]
-        public async Task<IActionResult> UpdateUserProject(Guid projectId, [FromBody] UpdateProjectDto project)
-        {
-            CustomHttpResponse<ReadProjectDto> response = await _projectService.UpdateProject(projectId, project);
-            return StatusCode((int)response.StatusCode, response.Data);
-        }
-
-        [HttpPatch("{projectId}")]
-        public async Task<IActionResult> PatchUserProject(Guid projectId, [FromBody] JsonPatchDocument<UpdateProjectDto> project)
-        {
-            CustomHttpResponse<ReadProjectDto> response = await _projectService.PatchProject(projectId, project);
-            return StatusCode((int)response.StatusCode, response.Data);
-        }
-
-        [HttpDelete("{projectId}")]
-        public async Task<IActionResult> DeleteUserProject(Guid projectId)
-        {
-            CustomHttpResponse<bool> response = await _projectService.DeleteProject(projectId);
             return StatusCode((int)response.StatusCode, response.Data);
         }
     }
--- TessoApi/TessoApi/Services/Interfaces/IProjectService.cs
+++ TessoApi/TessoApi/Services/Interfaces/IProjectService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.JsonPatch;
-using TessoApi.DTOs.Project;
+﻿using TessoApi.DTOs.Project;
 using TessoApi.Models.Http;
 
 namespace TessoApi.Services.Interfaces
@@ -9,8 +8,5 @@ namespace TessoApi.Services.Interfaces
         public Task<CustomHttpResponse<ReadProjectDto>> Create(CreateProjectDto project);
         public Task<CustomHttpResponse<ReadProjectDto>> GetProject(Guid projectId);
         public Task<CustomHttpResponse<List<ReadProjectDto>>> GetUserProjects(Guid userId);
-        public Task<CustomHttpResponse<ReadProjectDto>> UpdateProject(Guid projectId, UpdateProjectDto projectDto);
-        public Task<CustomHttpResponse<ReadProjectDto>> PatchProject(Guid projectId, JsonPatchDocument<UpdateProjectDto> projectPatch);
-        public Task<CustomHttpResponse<bool>> DeleteProject(Guid projectId);
     }
 }
